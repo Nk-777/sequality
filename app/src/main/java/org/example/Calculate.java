fb581d28810f8d0c464d927d7b9dfcb4274acd2d
naka <kenek0120@gmail.com>
2025-07-01 14:54:20 +0900

    Calculate class

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -5,4 +5,43 @@ public class Calculate {
         return x + y;
     }
     
+    public double average(int x,int y){
+        return (x + y) / 2.0;
+    }
+
+    public int sumBetween(int x, int y){
+        int sum = 0;
+        for(int i = x; i <= y; i++){
+            sum += i;
+        }
+        return sum;
+        
+    }
+
+    public int sumEvenBetween(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) sum += i;
+        }
+        return sum;
+    }
+
+    public int sumOddBetween(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 1) sum += i;
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Calculate cal = new Calculate();
+        System.out.println("Hello World!");
+        System.out.println("Sum of 2 and 3 is " + cal.sum(2, 3) + ". Average is " + cal.average(2, 3) + ".");
+        System.out.println("Sum of 1 to 10 is " + cal.sumBetween(1, 10) + ". Average is" + cal.average(1,10) + ".");
+        System.out.println("Sum of odd of 1 to 10 is " + cal.sumOddBetween(1, 10) + ".");
+        System.out.println("Sum of even of 1 to 10 is " + cal.sumEvenBetween(1, 10) + ".");
+    }
 }
+
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -3,14 +3,42 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class CalculateTest {
+
     @Test
-    public void testSum(){
-        Calculate calculate = new Calculate();
-        int expected = 5;
-        assertEquals(expected, calculate.sum(2,3));
-        
-    
-}
+    public void testSum_TwoAndThree_ReturnsFive() {
+        Calculate calc = new Calculate();
+        assertEquals(5, calc.sum(2, 3));
+    }
+
+    @Test
+    public void testAverage_TwoAndFour_ReturnsThree() {
+        Calculate calc = new Calculate();
+        assertEquals(3.0, calc.average(2, 4),0.0001);
+    }
+
+    @Test
+    public void testSumBetween_OneToFive_ReturnsFifteen() {
+        Calculate calc = new Calculate();
+        assertEquals(15, calc.sumBetween(1, 5));
+    }
+
+    @Test
+    public void testSumBetween_FiveToOne_ReturnsZero() {
+        Calculate calc = new Calculate();
+        assertEquals(0, calc.sumBetween(5, 1)); // ループが一度も実行されないパス
+    }
+
+
+    @Test
+    public void testSumEvenBetween_OneToTen_ReturnsThirty() {
+        Calculate calc = new Calculate();
+        assertEquals(30, calc.sumEvenBetween(1, 10));
+    }
+
+    @Test
+    public void testSumOddBetween_OneToTen_ReturnsTwentyFive() {
+        Calculate calc = new Calculate();
+        assertEquals(25, calc.sumOddBetween(1, 10));
+    }
 }
