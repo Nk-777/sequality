626a8225d8f3fcf5bd8fe9b5da3d1d78e19c6a67
naka <kenek0120@gmail.com>
2025-07-01 13:27:16 +0900

    スペース調整

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -15,6 +15,5 @@ public class AppTest {
     public void testAppReternHelloWorld(){
         App classUnderTest = new App();
         assertEquals("Hello World!",classUnderTest.getGreeting());
-
     }
 }
